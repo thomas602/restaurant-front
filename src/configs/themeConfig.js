e99cec0b8c2607d3e2ed2b5be34198a3f83f0b21
Thomas Reynoso <thomas.reynoso@codetria.com>
2022-05-25 16:15:17 -0300

    cambio el nombre y el logo de la app

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -3,7 +3,7 @@
 //Template config options
 const themeConfig = {
   app: {
-    appName: 'Vuexy',
+    appName: 'Resto',
     appLogoImage: require('@src/assets/images/logo/logo.svg').default
   },
   layout: {
